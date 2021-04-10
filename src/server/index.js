d96ddbbba1011a1744e1569746cdebeeacb205f2
Patrick <patrickhladun@gmail.com>
2021-04-10 14:00:30 +0100

    Refactor server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,26 +2,18 @@ const dotenv = require('dotenv');
 const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const app = express();
 dotenv.config();
-// process.env.MC_API_KEY
 
-const app = express()
+app.use(express.static('dist'));
 
-app.use(express.static('dist'))
-
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    // res.sendFile('dist/index.html');
+    res.sendFile(path.resolve('src/client/views/index.html'));
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get('/test', (req, res) => res.send(mockAPIResponse));
