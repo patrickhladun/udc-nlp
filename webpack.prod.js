b648f47a4cf460a7689af8f52e33eb4c1e8fcb48
Patrick <patrickhladun@gmail.com>
2021-04-13 07:57:37 +0100

    Add style-loader to webpack dev config

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,7 @@ module.exports = {
             {   
                 test: '/\.css$/',
                 exclude: /node_modules/,
-                use: ['css-loader']
+                use: ['style-loader', 'css-loader']
             }
         ]
     },
